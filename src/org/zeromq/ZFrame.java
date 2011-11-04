faecb13a429352b8dde2dfa28469a12c13b5ec19
Arkadiusz Orzechowski <aorzecho@gmail.com>
2011-11-04 15:07:29 +0100

    made ZFrame(byte[]) constructor public

--- src/org/zeromq/ZFrame.java
+++ src/org/zeromq/ZFrame.java
@@ -39,7 +39,7 @@ public class ZFrame {
 	 * @param data
 	 * 			Data to copy into ZFrame object
 	 */
-	protected ZFrame(byte[] data)
+	public ZFrame(byte[] data)
 	{
 		if (data != null) {
 			this.data = (byte[]) data.clone();
